fddecb564a175bd03e637b9486c5da05fa79af71
520snow <coding_sun@163.com>
2023-01-30 16:01:22 +0800

    test

--- docs/latest/fiddles/system/clipboard/paste/renderer.js
+++
@@ -1,7 +0,0 @@
-const pasteBtn = document.getElementById('paste-to')
-
-pasteBtn.addEventListener('click', async () => {
-  await clipboard.writeText('What a demo!')
-  const message = `Clipboard contents: ${await clipboard.readText()}`
-  document.getElementById('paste-from').innerHTML = message
-})
--- src/pages/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Home from './home/_index';
-
-export default function HomeWrapper() {
-  return <Home />;
-}
